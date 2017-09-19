2288383f8eb930e6bdf590915b0c932039f47b1c
tangentguo(郭迅) <tangentguo@tencent.com>
2017-09-19 10:27:19 +0800

    统计结果

--- cy_game/js/Gamepage.js
+++ cy_game/js/Gamepage.js
@@ -17,7 +17,8 @@ class Gamepage extends React.Component {
             group3:"第4组",
             end:false,
             text:"点击开始",
-            correct:"0/0"
+            correct:"0/0",
+            buttonShow:false
         }
         this.result = [[],[],[],[]]
         this.resultTime = []
@@ -25,13 +26,20 @@ class Gamepage extends React.Component {
 
     select=(n)=>{
         this.n =n;
-        this.count = {right:0,total:0}
-        this.cyIndex = 0;
-        this.timerID = null
-        clearInterval(this.timerID)
-        this.running = false
+        this.oneEnd()
         this.data[n] = cyArr.slice(n*this.step,(n+1)*this.step)
         this.setState({timeout:0,text:"点击开始"})
+    }
+
+    oneEnd = ()=>{
+        this.count = {right:0,total:0}
+        this.cyIndex = 0;
+        clearInterval(this.timerID)
+        this.running = false
+        this.setState({buttonShow:false})
+
+        console.log(this.result,this.resultTime)
+
     }
 
     start=()=>{
@@ -41,6 +49,7 @@ class Gamepage extends React.Component {
         }
 
         if(this.running) return
+        this.setState({buttonShow:1})
         this.timer()
         this.right()
         this.running = true
@@ -48,6 +57,7 @@ class Gamepage extends React.Component {
 
     timer= ()=>{
         this.timerID = setInterval(()=>{
+
             var t = ++this.state.timeout
             if(t<60){
                 this.setState({timeout:t})
@@ -66,7 +76,6 @@ class Gamepage extends React.Component {
             return
         }
 
-
         if(this.cyIndex>data.length-1){
             clearInterval(this.timerID)
         }else{
@@ -84,6 +93,23 @@ class Gamepage extends React.Component {
 
     endopen = ()=>{
         this.setState({endOpen:!this.state.endOpen})
+        var elm = document.querySelector("#result_show_box")
+        var s = ""
+        this.result.forEach((el,n)=>{
+            var len = el.length
+            var correct = 0
+            s+="<ul>"
+            s+=`<li>第${n+1}组 用时${this.resultTime[n]}s</li>`
+            el.forEach(v=>{
+                if(v.correct) correct++
+                s+= `<li class="${v.correct?"":"red"}">${v.text}</li>`
+            })
+            if(len) s+=`<li>正确率：${(correct/len).toFixed(2)}<br/>平均用时${(this.resultTime[n]/len).toFixed(2)}</li>`
+            s+="</ul>"
+        })
+
+        elm.innerHTML = s
+
     }
 
     render() {
@@ -102,14 +128,15 @@ class Gamepage extends React.Component {
                     正确/总数 {this.state.correct}
                 </div>
                 <div className="text" onClick={this.start}>{this.state.text}</div>
-                <div className="btn">
+                <div className={"btn "+(this.state.buttonShow?"":"hidden")}>
                     <button style={{background:"green"}} onClick={this.right.bind(this,1)}>正确√</button>
                     <button onClick={this.right.bind(this,0)}>跳过×</button>
+                    <button onClick={this.oneEnd}>完成</button>
                 </div>
 
                 <div className="result" onClick={this.endopen.bind(this,true)}>END</div>
-                <div className={"result_show "+(this.state.endOpen?"":"hidden")}>
-
+                <div id="result_show" className={"result_show "+(this.state.endOpen?"":"hidden")}>
+                    <div className="result_show_box" id="result_show_box"></div>
                 </div>
             </div>
         )
