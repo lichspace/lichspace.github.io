8e477275dc67f0df076fd47ef3df9e731e3c2fdd
tangentguo mac <guoxun01@126.com>
2017-09-24 21:41:55 +0800

    修改3

--- cy_game/js/FirstPage.js
+++ cy_game/js/FirstPage.js
@@ -1,7 +1,8 @@
 function FirstPage(props){
     return(
         <div className={"FirstPage "+(props.show?"":"hidden")}>
-            <p className="yindao">产品研发中心<span>Rund2成语接龙游戏</span>— just play —</p>
+            <p>大屏幕显示的词或成语，猜中即可，猜中</p>
+            <p className="yindao">产品研发中心<span>Rund2猜猜接龙游戏</span>— just play —</p>
             <div className="start-button" onClick={props.start}>START</div>
         </div>
 
