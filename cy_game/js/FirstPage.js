b6db456731a4ff3065bf4d8ff468b686d47e00d4
tangentguo mac <guoxun01@126.com>
2017-09-25 08:39:31 +0800

    哈哈

--- cy_game/js/FirstPage.js
+++ cy_game/js/FirstPage.js
@@ -1,7 +1,7 @@
 function FirstPage(props){
     return(
         <div className={"FirstPage "+(props.show?"":"hidden")}>
-            <p>大屏幕显示的词或成语，猜中即可，猜中</p>
+            <p>大屏幕显示的词或成语，限时3分钟，每组30个，猜中多者为胜</p>
             <p className="yindao">产品研发中心<span>Rund2猜猜接龙游戏</span>— just play —</p>
             <div className="start-button" onClick={props.start}>START</div>
         </div>
